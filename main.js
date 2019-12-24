bc2db878a0e0ba5548ca65bd12603f8c9782614c
Wayne Zhu <way87767@gmail.com>
2019-12-24 17:01:14 -0500

    beginning work on refactoring

--- main.js
+++ main.js
@@ -24,12 +24,11 @@ function generateBoard(element){
 
 class ChessGame{
     constructor(){
-        this.current = "white";
         this.game_playing = false;
-        this.board = [];
-        this.currentPlayer = "white";
-        this.currentSelected = null;
-        this.currentMoves = [];
+        this.board = []; //current state of the 8x8 board
+        this.currentPlayer = "white"; //the current player white or black
+        this.currentSelected = null; //the currently selected piece
+        this.currentMoves = []; //list of all possible moves in format "[[i, j], [i, j]]
         
         for(var i = 0; i < 8; i++){
             var row = [];
@@ -138,136 +137,48 @@ class ChessGame{
         }
     }
     
-    onClick(id){
-        var piece = this.board[id[0]][id[1]];
-        var currentLocation = String(id[0]) + String(id[1]);
-        
-        if(this.currentSelected == null){ //if no piece is selected right now
-            
-            if(piece != null){ //if this tile contains a game piece
-                var moves = this.getValidMoves(id);
-                
-                for(var i = 0; i < 8; i++){
-
-                    for(var j = 0; j < 8; j++){
-
-                        var thisID = String(i) + String(j)
-                        var tile = document.getElementById(thisID);
-
-                        if(moves.includes(thisID)){
-                            tile.setAttribute("data-selected", "true");
-                        } 
-                        else{
-                            tile.setAttribute("data-selected", "false");
-                        }
-                    }
-                }
-                this.currentSelected = piece;
-                this.currentMoves = moves;
-            }
-        }
-        else{
-            
-            if(piece == null){
-                if(this.currentMoves.includes(currentLocation)){
-                    this.movePiece(this.currentSelected, id[0], id[1]);
-                    this.deselectAll();
-                    this.nextTurn();
-                    this.draw();
-                }
-                else{
-                    this.deselectAll();   
-                }
-            }
-            else if(piece == this.currentSelected){ //deselect if selected again
-                this.deselectAll();
-            }
-            else if(piece.color == this.currentPlayer){ //if piece is an friendly piece, select that instead
-                this.deselectAll();
-                var moves = this.getValidMoves(id);
-                
-                for(var i = 0; i < 8; i++){
-
-                    for(var j = 0; j < 8; j++){
-
-                        var thisID = String(i) + String(j)
-                        var tile = document.getElementById(thisID);
-
-                        if(moves.includes(thisID)){
-                            tile.setAttribute("data-selected", "true");
-                        } 
-                        else{
-                            tile.setAttribute("data-selected", "false");
-                        }
-                    }
-                }
-                this.currentSelected = piece;
-                this.currentMoves = moves;                
-            }
-            else if(piece.color != this.currentPlayer){ //if we're trying to attack an enemy
-                piece.alive = false;
-                this.board[id[0]][id[1]] = null;
-                this.movePiece(this.currentSelected, id[0], id[1]);
-                this.deselectAll();
-                this.nextTurn();
-                this.draw();
-            }
-            
-            else{ //otherwise deselect all
-                this.deselectAll();
-            }
-        }
-    }
-    
-    deselectAll(){ //remove all blue selection tiles
-        for(var i = 0; i < 8; i++){
-            for(var j = 0; j < 8; j++){
-                var thisID = String(i) + String(j)
-                var tile = document.getElementById(thisID);
-                tile.setAttribute("data-selected", "false");
-            }
-        }
-        this.currentSelected = null;
-        this.currentMoves = null;
-    }
-    
-    getValidMoves(id){ //get all possible squares the piece at i, j can move to
-        var i = id[0];
-        var j = id[1];
+    onClick(id){ //sends id in "ij" format
+        var i = parseInt(id[0]);
+        var j = parseInt(id[1]);
         var piece = this.board[i][j];
         
-        var validMoves = [];
-        if(piece != null && piece.color == this.currentPlayer){
-            var moves = piece.getValidMoves(this);
-            for(var i = 0; i < moves.length; i++){
-                var move = moves[i];
-                var curr = this.board[move[0]][move[1]];
-                if(curr == null){
-                    validMoves.push(move);
-                }
-                else if(curr.color != this.currentPlayer){
-                    validMoves.push(move);
+        console.log(id);
+        console.log(this.currentSelected);
+        console.log(piece);
+        
+        if(this.currentSelected == null){ //if no piece is selected
+            if(piece != null){ //if this tile contains a piece...
+                if(piece.color == this.currentPlayer){  //...and it belongs to the current player
+                    piece.select();
+                    this.currentSelected = piece;
                 }
             }
-            return validMoves;
         }
-        else{
-            return [];
+        else{ //if a piece is selected perform a possible movement or deselection
+            var values = piece.move(i, j); //attempt to make a move
+            if(values == false){ //deselect if move failed
+                piece.deselect();
+            }
+            else{
+                this.movePiece(piece, i, j);
+            }
+            this.currentSelected = null;
         }
     }
     
     movePiece(piece, newI, newJ){
-        var oldI = piece.i;
-        var oldJ = piece.j;
-        piece.i = parseInt(newI);
-        piece.j = parseInt(newJ);
-        piece.firstMove = false;
-        
-        this.board[oldI][oldJ] = null;
-        this.board[newI][newJ] = piece;
+    
     }
     
     checkForMate(){
+        
+    }
+    
+    setValidMoves(){
+        
+    }
+    
+    getValidMoves(){
         
     }
     
@@ -282,324 +193,32 @@ class ChessGame{
         return (this.board[i][j].color != this.currentPlayer);
     }
     
+    isFriend(i, j){
+        if(this.board[i][j] == null){
+            return false;
+        }
+        return (this.board[i][j].color == this.currentPlayer);
+    }
+    
     nextTurn(){
-        console.log(this.currentPlayer);
         if(this.currentPlayer == "white"){
             this.currentPlayer = "black";
         }
         else{
             this.currentPlayer = "white";
         }
-        console.log(this.currentPlayer);
     }
 
-}
-
-class Piece{
-    constructor(game, color, i, j){
-        this.game = game;
-        this.color = color;
-        this.alive = true;
-        this.i = i; 
-        this.j = j;
-        this.firstMove = true;
-        this.validMoves = []; //gets all valid moves on this term in "ij" format
-    }
-    
-    getAllMoves(game){
-        return;
-    }
-    
-    getValidMoves(game){
-        var all_moves = this.getAllMoves(game);
-        var validMoves = [];
-        for(var x = 0; x < all_moves.length; x++){
-            var move = all_moves[x];
-            var i = move[0];
-            var j = move[1];
-            if(within_range(i, j)){
-                var strMove = String(i) + String(j);
-                validMoves.push(strMove);
-            }
-        }
-        return validMoves;
-    }
-}
-
-class Pawn extends Piece{
-    constructor(game, color, i, j){
-        super(game, color, i, j);
-        this.firstMove = true;
-    }
-    
-    getValidMoves(game){
-        var all_moves = [];
-        if(this.color == "white"){
-            if(!game.hasPiece(this.i + 1, this.j)){
-                all_moves = [[this.i + 1, this.j]];
-            }
-            if(this.firstMove && !game.hasPiece(this.i + 1, this.j) && !game.hasPiece(this.i + 2, this.j)){
-                all_moves.push([this.i + 2, this.j]);
-            }
-            if(game.isEnemy(this.i + 1, this.j - 1)){
-                all_moves.push([this.i + 1, this.j - 1]);
-            }
-            if(game.isEnemy(this.i + 1, this.j + 1)){
-                all_moves.push([this.i + 1, this.j + 1]);
-            }
-        }
-        else{
-            if(!game.hasPiece(this.i - 1, this.j)){
-                all_moves = [[this.i - 1, this.j]];
-            }
-            if(this.firstMove && !game.hasPiece(this.i - 1, this.j) && !game.hasPiece(this.i - 2, this.j)){
-                all_moves.push([this.i - 2, this.j]);
-            }
-            if(game.isEnemy(this.i - 1, this.j - 1)){
-                all_moves.push([this.i - 1, this.j - 1]);
-            }
-            if(game.isEnemy(this.i - 1, this.j + 1)){
-                all_moves.push([this.i - 1, this.j + 1]);
-            }
-        }
-        return all_moves;
-        
-    }
-}
-
-class Rook extends Piece{
-    constructor(game, color, i, j){
-        super(game, color, i, j);
-        this.validMoves = [];
-    }
-    
-    getValidMoves(game){
-        var all_moves = [];
-        for(var j = this.j - 1; j >= 0; j -= 1){
-            if(game.hasPiece(this.i, j)){
-                if(game.isEnemy(this.i, j)){
-                    all_moves.push([this.i, j]);
-                }
-                break;
-            }
-            all_moves.push([this.i, j]);
-        }
-        for(var j = this.j + 1; j < 8; j++){
-            if(game.hasPiece(this.i, j)){
-                if(game.isEnemy(this.i, j)){
-                    all_moves.push([this.i, j]);
-                }
-                break;
-            }
-            console.log(this.i, j)
-            console.log(game.hasPiece(this.i, j))
-            all_moves.push([this.i, j]);
-        }
-        
-        for(var i = this.i - 1; i >= 0; i -= 1){
-            if(game.hasPiece(i, this.j)){
-                if(game.isEnemy(i, this.j)){
-                    all_moves.push([i, this.j]);
-                }
-                break;
-            }
-            all_moves.push([i, this.j]);
-        }
-        for(var i = this.i + 1; i < 8; i++){
-            if(game.hasPiece(i, this.j)){
-                if(game.isEnemy(i, this.j)){
-                    all_moves.push([i, this.j]);
-                }
-                break;
-            }            
-            all_moves.push([i, this.j]);
-        }
-        console.log(all_moves);
-        return all_moves;
-    }
-}
-
-class Knight extends Piece{
-    constructor(color, i, j){
-        super(color, i, j);
-    }
-    
-    getAllMoves(game){
-        var all_moves = [[this.i - 2, this.j + 1], [this.i - 1, this.j + 2], [this.i + 1, this.j + 2], [this.i + 2, this.j + 1], [this.i + 2, this.j - 1], [this.i + 1, this.j - 2], [this.i - 1, this.j - 2], [this.i - 2, this.j - 1]];
-        return all_moves;
-    }
-}
-
-class Bishop extends Piece{
-    constructor(color, i, j){
-        super(color, i, j);
-    }
-    
-    getAllMoves(game){
-        var all_moves = [];
-        
-        for(var z = 1; z < 8; z++){ //down and right
-            if(!within_range(this.i + z, this.j + z)){
-                break;
-            }
-            else if(game.hasPiece(this.i + z, this.j + z)){
-                if(game.isEnemy(this.i + z, this.j + z)){
-                    all_moves.push([this.i + z, this.j + z]);
-                }
-                break;
-            }
-            all_moves.push([this.i + z, this.j + z]);
-        }
-        for(var z = 1; z < 8; z++){
-            if(!within_range(this.i - z, this.j + z)){
-                break;
-            }
-            else if(game.hasPiece(this.i - z, this.j + z)){
-                if(game.isEnemy(this.i - z, this.j + z)){
-                    all_moves.push([this.i - z, this.j + z]);
-                }
-                break;
-            }
-            all_moves.push([this.i - z, this.j + z]);
-        }
-        for(var z = 1; z < 8; z++){
-            if(!within_range(this.i + z, this.j - z)){
-                break;
-            }
-            else if(game.hasPiece(this.i + z, this.j - z)){
-                if(game.isEnemy(this.i + z, this.j - z)){
-                    all_moves.push([this.i + z, this.j - z]);
-                }
-                break;
-            }
-            all_moves.push([this.i + z, this.j - z]);
-        }
-        for(var z = 1; z < 8; z++){
-            if(!within_range(this.i - z, this.j - z)){
-                break;
-            }
-            else if(game.hasPiece(this.i - z, this.j - z)){
-                if(game.isEnemy(this.i - z, this.j - z)){
-                    all_moves.push([this.i - z, this.j - z]);
-                }
-                break;
-            }
-            all_moves.push([this.i - z, this.j - z]);
-        }
-        return all_moves;
-    }
-}
-
-class Queen extends Piece{
-    constructor(color, i, j){
-        super(color, i, j);
-    }
-    
-    getAllMoves(game){
-        var all_moves = [];
-        
-        for(var j = this.j - 1; j >= 0; j -= 1){
-            if(game.hasPiece(this.i, j)){
-                if(game.isEnemy(this.i, j)){
-                    all_moves.push([this.i, j]);
-                }
-                break;
-            }
-            all_moves.push([this.i, j]);
-        }
-        for(var j = this.j + 1; j < 8; j++){
-            if(game.hasPiece(this.i, j)){
-                if(game.isEnemy(this.i, j)){
-                    all_moves.push([this.i, j]);
-                }
-                break;
-            }
-            all_moves.push([this.i, j]);
-        }
-        
-        for(var i = this.i - 1; i >= 0; i -= 1){
-            if(game.hasPiece(i, this.j)){
-                if(game.isEnemy(i, this.j)){
-                    all_moves.push([i, this.j]);
-                }
-                break;
-            }
-            all_moves.push([i, this.j]);
-        }
-        for(var i = this.i + 1; i < 8; i++){
-            if(game.hasPiece(i, this.j)){
-                if(game.isEnemy(i, this.j)){
-                    all_moves.push([i, this.j]);
-                }
-                break;
-            }            
-            all_moves.push([i, this.j]);
-        }
-        
-        for(var z = 1; z < 8; z++){ //down and right
-            if(!within_range(this.i + z, this.j + z)){
-                break;
-            }
-            else if(game.hasPiece(this.i + z, this.j + z)){
-                if(game.isEnemy(this.i + z, this.j + z)){
-                    all_moves.push([this.i + z, this.j + z]);
-                }
-                break;
-            }
-            all_moves.push([this.i + z, this.j + z]);
-        }
-        for(var z = 1; z < 8; z++){
-            if(!within_range(this.i - z, this.j + z)){
-                break;
-            }
-            else if(game.hasPiece(this.i - z, this.j + z)){
-                if(game.isEnemy(this.i - z, this.j + z)){
-                    all_moves.push([this.i - z, this.j + z]);
-                }
-                break;
-            }
-            all_moves.push([this.i - z, this.j + z]);
-        }
-        for(var z = 1; z < 8; z++){
-            if(!within_range(this.i + z, this.j - z)){
-                break;
-            }
-            else if(game.hasPiece(this.i + z, this.j - z)){
-                if(game.isEnemy(this.i + z, this.j - z)){
-                    all_moves.push([this.i + z, this.j - z]);
-                }
-                break;
-            }
-            all_moves.push([this.i + z, this.j - z]);
-        }
-        for(var z = 1; z < 8; z++){
-            if(!within_range(this.i - z, this.j - z)){
-                break;
-            }
-            else if(game.hasPiece(this.i - z, this.j - z)){
-                if(game.isEnemy(this.i - z, this.j - z)){
-                    all_moves.push([this.i - z, this.j - z]);
-                }
-                break;
-            }
-            all_moves.push([this.i - z, this.j - z]);
-        }
-        
-        return all_moves;
-    }
-}
-
-class King extends Piece{
-    constructor(color, i, j){
-        super(color, i, j);
-    }
-    
-    getAllMoves(game){
-        var all_moves = [[this.i + 1, this.j], [this.i - 1, this.j], [this.i, this.j - 1], [this.i, this.j + 1]];
-        return all_moves;
-    }
 }
                
 function within_range(i, j){
     return (0 <= i && i <= 7 && 0 <= j && j <= 7);
+}
+
+function fromString(str){ //returns in [i, j] format
+    return [str[0], str[1]];
+}
+
+function toString(lst){ //returns in "ij" format
+    return String(lst[0]) + String(lst[1]);
 }
---
+++ pieces.js
@@ -0,0 +1,317 @@
+class Piece{
+    constructor(game, color, i, j){
+        this.game = game;
+        this.color = color; //color of this piece white or black
+        this.alive = true; //if this piece is uncaptured
+        this.i = i; 
+        this.j = j;
+        this.firstMove = true;
+        this.validMoves = []; //gets all valid moves on this term in "[i, j]" format
+    }
+    
+    move(i, j){ //returns new [i, j] if moved, otherwise return false
+        if(this.game.isFriend(i, j)){
+            return false
+        }
+    }
+    
+    select(){
+        for(var i = 0; i < this.validMoves.length; i++){
+            var id = this.validMoves[i]
+            var element = document.getElementById(toString(id));
+            element.setAttribute("data-selected", "true");
+        }
+        console.log('select');
+    }
+    
+    deselect(){
+        for(var i = 0; i < this.validMoves.length; i++){
+            var id = this.validMoves[i]
+            var element = document.getElementById(toString(id));
+            element.setAttribute("data-selected", "false");
+        }
+    }
+    
+    getValidMoves(game){ //returns valid move in [i, j] format, removing off--board choices
+        var allMoves = this.getAllMoves(game);
+        var validMoves = []
+        for(var x = 0; x < allMoves.length; x++){
+            var curr = allMoves[x];
+            if(within_range(curr[0], curr[1])){
+                validMoves.push(curr);
+            }
+        }
+        this.validMoves = validMoves;
+    }
+}
+
+class Pawn extends Piece{
+    constructor(game, color, i, j){
+        super(game, color, i, j);
+    }
+    
+    getAllMoves(game){ //returns moves in [i, j] format
+        var all_moves = [];
+        if(this.color == "white"){
+            if(!game.hasPiece(this.i + 1, this.j)){
+                all_moves = [[this.i + 1, this.j]];
+                if(this.firstMove && !game.hasPiece(this.i + 2, this.j)){
+                    all_moves.push([this.i + 2, this.j]);
+                }
+            }
+            if(game.isEnemy(this.i + 1, this.j - 1)){
+                all_moves.push([this.i + 1, this.j - 1]);
+            }
+            if(game.isEnemy(this.i + 1, this.j + 1)){
+                all_moves.push([this.i + 1, this.j + 1]);
+            }
+        }
+        else{
+            if(!game.hasPiece(this.i - 1, this.j)){
+                all_moves = [[this.i - 1, this.j]];
+                if(this.firstMove && !game.hasPiece(this.i - 2, this.j)){
+                    all_moves.push([this.i - 2, this.j]);
+                }
+            }
+            if(game.isEnemy(this.i - 1, this.j - 1)){
+                all_moves.push([this.i - 1, this.j - 1]);
+            }
+            if(game.isEnemy(this.i - 1, this.j + 1)){
+                all_moves.push([this.i - 1, this.j + 1]);
+            }
+        }
+        return all_moves;
+        
+    }
+}
+
+class Rook extends Piece{
+    constructor(game, color, i, j){
+        super(game, color, i, j);
+    }
+    
+    getAllMoves(game){
+        var all_moves = [];
+        for(var j = this.j - 1; j >= 0; j -= 1){
+            if(game.hasPiece(this.i, j)){
+                if(game.isEnemy(this.i, j)){
+                    all_moves.push([this.i, j]);
+                }
+                break;
+            }
+            all_moves.push([this.i, j]);
+        }
+        for(var j = this.j + 1; j < 8; j++){
+            if(game.hasPiece(this.i, j)){
+                if(game.isEnemy(this.i, j)){
+                    all_moves.push([this.i, j]);
+                }
+                break;
+            }
+            console.log(this.i, j)
+            console.log(game.hasPiece(this.i, j))
+            all_moves.push([this.i, j]);
+        }
+        
+        for(var i = this.i - 1; i >= 0; i -= 1){
+            if(game.hasPiece(i, this.j)){
+                if(game.isEnemy(i, this.j)){
+                    all_moves.push([i, this.j]);
+                }
+                break;
+            }
+            all_moves.push([i, this.j]);
+        }
+        for(var i = this.i + 1; i < 8; i++){
+            if(game.hasPiece(i, this.j)){
+                if(game.isEnemy(i, this.j)){
+                    all_moves.push([i, this.j]);
+                }
+                break;
+            }            
+            all_moves.push([i, this.j]);
+        }
+        return all_moves;
+    }
+}
+
+class Knight extends Piece{
+    constructor(color, i, j){
+        super(color, i, j);
+    }
+    
+    getAllMoves(game){
+        var all_moves = [[this.i - 2, this.j + 1], [this.i - 1, this.j + 2], [this.i + 1, this.j + 2], [this.i + 2, this.j + 1], [this.i + 2, this.j - 1], [this.i + 1, this.j - 2], [this.i - 1, this.j - 2], [this.i - 2, this.j - 1]];
+        return all_moves;
+    }
+}
+
+class Bishop extends Piece{
+    constructor(color, i, j){
+        super(color, i, j);
+    }
+    
+    getAllMoves(game){
+        var all_moves = [];
+        
+        for(var z = 1; z < 8; z++){ //down and right
+            if(!within_range(this.i + z, this.j + z)){
+                break;
+            }
+            else if(game.hasPiece(this.i + z, this.j + z)){
+                if(game.isEnemy(this.i + z, this.j + z)){
+                    all_moves.push([this.i + z, this.j + z]);
+                }
+                break;
+            }
+            all_moves.push([this.i + z, this.j + z]);
+        }
+        for(var z = 1; z < 8; z++){
+            if(!within_range(this.i - z, this.j + z)){
+                break;
+            }
+            else if(game.hasPiece(this.i - z, this.j + z)){
+                if(game.isEnemy(this.i - z, this.j + z)){
+                    all_moves.push([this.i - z, this.j + z]);
+                }
+                break;
+            }
+            all_moves.push([this.i - z, this.j + z]);
+        }
+        for(var z = 1; z < 8; z++){
+            if(!within_range(this.i + z, this.j - z)){
+                break;
+            }
+            else if(game.hasPiece(this.i + z, this.j - z)){
+                if(game.isEnemy(this.i + z, this.j - z)){
+                    all_moves.push([this.i + z, this.j - z]);
+                }
+                break;
+            }
+            all_moves.push([this.i + z, this.j - z]);
+        }
+        for(var z = 1; z < 8; z++){
+            if(!within_range(this.i - z, this.j - z)){
+                break;
+            }
+            else if(game.hasPiece(this.i - z, this.j - z)){
+                if(game.isEnemy(this.i - z, this.j - z)){
+                    all_moves.push([this.i - z, this.j - z]);
+                }
+                break;
+            }
+            all_moves.push([this.i - z, this.j - z]);
+        }
+        return all_moves;
+    }
+}
+
+class Queen extends Piece{
+    constructor(color, i, j){
+        super(color, i, j);
+    }
+    
+    getAllMoves(game){
+        var all_moves = [];
+        
+        for(var j = this.j - 1; j >= 0; j -= 1){
+            if(game.hasPiece(this.i, j)){
+                if(game.isEnemy(this.i, j)){
+                    all_moves.push([this.i, j]);
+                }
+                break;
+            }
+            all_moves.push([this.i, j]);
+        }
+        for(var j = this.j + 1; j < 8; j++){
+            if(game.hasPiece(this.i, j)){
+                if(game.isEnemy(this.i, j)){
+                    all_moves.push([this.i, j]);
+                }
+                break;
+            }
+            all_moves.push([this.i, j]);
+        }
+        
+        for(var i = this.i - 1; i >= 0; i -= 1){
+            if(game.hasPiece(i, this.j)){
+                if(game.isEnemy(i, this.j)){
+                    all_moves.push([i, this.j]);
+                }
+                break;
+            }
+            all_moves.push([i, this.j]);
+        }
+        for(var i = this.i + 1; i < 8; i++){
+            if(game.hasPiece(i, this.j)){
+                if(game.isEnemy(i, this.j)){
+                    all_moves.push([i, this.j]);
+                }
+                break;
+            }            
+            all_moves.push([i, this.j]);
+        }
+        
+        for(var z = 1; z < 8; z++){ //down and right
+            if(!within_range(this.i + z, this.j + z)){
+                break;
+            }
+            else if(game.hasPiece(this.i + z, this.j + z)){
+                if(game.isEnemy(this.i + z, this.j + z)){
+                    all_moves.push([this.i + z, this.j + z]);
+                }
+                break;
+            }
+            all_moves.push([this.i + z, this.j + z]);
+        }
+        for(var z = 1; z < 8; z++){
+            if(!within_range(this.i - z, this.j + z)){
+                break;
+            }
+            else if(game.hasPiece(this.i - z, this.j + z)){
+                if(game.isEnemy(this.i - z, this.j + z)){
+                    all_moves.push([this.i - z, this.j + z]);
+                }
+                break;
+            }
+            all_moves.push([this.i - z, this.j + z]);
+        }
+        for(var z = 1; z < 8; z++){
+            if(!within_range(this.i + z, this.j - z)){
+                break;
+            }
+            else if(game.hasPiece(this.i + z, this.j - z)){
+                if(game.isEnemy(this.i + z, this.j - z)){
+                    all_moves.push([this.i + z, this.j - z]);
+                }
+                break;
+            }
+            all_moves.push([this.i + z, this.j - z]);
+        }
+        for(var z = 1; z < 8; z++){
+            if(!within_range(this.i - z, this.j - z)){
+                break;
+            }
+            else if(game.hasPiece(this.i - z, this.j - z)){
+                if(game.isEnemy(this.i - z, this.j - z)){
+                    all_moves.push([this.i - z, this.j - z]);
+                }
+                break;
+            }
+            all_moves.push([this.i - z, this.j - z]);
+        }
+        
+        return all_moves;
+    }
+}
+
+class King extends Piece{
+    constructor(color, i, j){
+        super(color, i, j);
+    }
+    
+    getAllMoves(game){
+        var all_moves = [[this.i + 1, this.j], [this.i - 1, this.j], [this.i, this.j - 1], [this.i, this.j + 1]];
+        return all_moves;
+    }
+}
